1e224592e4eb9a26a4bb3ae8e206e1acfe880c49
Rolf1e <tigran.slama@contentsquare.com>
2024-08-28 11:11:01 +0200

    feat(nvim): java lsp updates

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,78 +1,126 @@
-local softwares_dir_path = '/media/rolfie/ssd2/softwares'
+local home = os.getenv('HOME')
+local jdtls = require('jdtls')
+local jdtls_version = '1.37.0'
 
-local jdtls_path = softwares_dir_path .. '/jdtls'
-local equinox_version = '1.6.400.v20210924-0641'
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = softwares_dir_path .. '/jdtls/.workspace/' .. project_name
+-- File types that signify a Java project's root directory. This will be
+-- used by eclipse to determine what constitutes a workspace
+local root_markers = {'gradlew', 'mvnw', '.git'}
+local root_dir = require('jdtls.setup').find_root(root_markers)
 
--- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+-- eclipse.jdt.ls stores project specific data within a folder. If you are working
+-- with multiple different projects, each project must use a dedicated data directory.
+-- This variable is used to configure eclipse to use the directory name of the
+-- current project found using the root_marker as the folder for project specific data.
+local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
-local m2_path = '/home/rolfie/.m2'
-local lombok_version = '1.18.22'
-local lombok_path = m2_path .. '/repository/org/projectlombok/lombok/' .. lombok_version .. '/lombok-' .. lombok_version  .. '.jar'
-local jdtls_config = {
-  -- The command that starts the language server
+
+local on_attach = function(client, bufnr)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set("n", "<leader>o", jdtls.organize_imports, bufopts)
+end
+
+local config = {
+  flags = {
+    debounce_text_changes = 80,
+  },
+  on_attach = on_attach,  -- We pass our on_attach keybindings to the configuration map
+  root_dir = root_dir, -- Set the root directory to our found root_marker
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- These are defined by the eclipse.jdt.ls project and will be passed to eclipse when starting.
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      contentProvider = { preferred = 'fernflower' },  -- Use fernflower to decompile library code
+      -- Specify any completion options
+      completion = {
+        favoriteStaticMembers = {
+          "org.hamcrest.MatcherAssert.assertThat",
+          "org.hamcrest.Matchers.*",
+          "org.hamcrest.CoreMatchers.*",
+          "org.junit.jupiter.api.Assertions.*",
+          "java.util.Objects.requireNonNull",
+          "java.util.Objects.requireNonNullElse",
+          "org.mockito.Mockito.*"
+        },
+        filteredTypes = {
+          "com.sun.*",
+          "io.micrometer.shaded.*",
+          "java.awt.*",
+          "jdk.*", "sun.*",
+        },
+      },
+      -- Specify any options for organizing imports
+      sources = {
+        organizeImports = {
+          starThreshold = 9999;
+          staticStarThreshold = 9999;
+        },
+      },
+      -- How code generation should act
+      codeGeneration = {
+        toString = {
+          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+        },
+        hashCodeEquals = {
+          useJava7Objects = true,
+        },
+        useBlocks = true,
+      },
+      -- If you are developing in projects with different Java versions, you need
+      -- to tell eclipse.jdt.ls to use the location of the JDK for your Java version
+      -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+      -- And search for `interface RuntimeOption`
+      -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
+      configuration = {
+        runtimes = {
+          {
+            name = "JavaSE-21",
+            path = home .. "/.sdkman/candidates/java/21.0.2-open",
+
+          },
+          {
+            name = "JavaSE-11",
+            path = home .. "/.sdkman/candidates/java/11.0.24-amzn",
+          }
+        }
+      }
+    }
+  },
+  -- cmd is the command that starts the language server. Whatever is placed
+  -- here is what is passed to the command line to execute jdtls.
+  -- Note that eclipse.jdt.ls must be started with a Java version of 17 or higher
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  -- for the full list of options
   cmd = {
-
-    -- 💀
-    'java', -- or '/path/to/java17_or_newer/bin/java'
-            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
-
+    home .. "/.sdkman/candidates/java/21.0.2-open/bin/java",
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
     '-Dosgi.bundles.defaultStartLevel=4',
     '-Declipse.product=org.eclipse.jdt.ls.core.product',
     '-Dlog.protocol=true',
     '-Dlog.level=ALL',
-    '-Xms1g',
+    '-Xmx4g',
     '--add-modules=ALL-SYSTEM',
     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-javaagent:' .. lombok_path,
+    -- If you use lombok, download the lombok jar and place it in ~/.local/share/eclipse
+    -- '-javaagent:' .. home .. '/.local/share/eclipse/lombok.jar',
 
-    -- 💀
-    '-jar', jdtls_path .. '/plugins/org.eclipse.equinox.launcher_'.. equinox_version ..'.jar',
-         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-         -- Must point to the                                                     Change this to
-         -- eclipse.jdt.ls installation                                           the actual version
+    -- The jar file is located where jdtls was installed. This will need to be updated
+    -- to the location where you installed jdtls
+    '-jar', vim.fn.glob('/opt/homebrew/Cellar/jdtls/' .. jdtls_version .. '/libexec/plugins/org.eclipse.equinox.launcher_*.jar'),
 
+    -- The configuration for jdtls is also placed where jdtls was installed. This will
+    -- need to be updated depending on your environment
+    '-configuration', '/opt/homebrew/Cellar/jdtls/' .. jdtls_version .. '/libexec/config_mac',
 
-    -- 💀
-    '-configuration', jdtls_path .. '/config_linux',
-                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
-                    -- eclipse.jdt.ls installation            Depending on your system.
-
-
-    -- 💀
-    -- See `data directory configuration` section in the README
-    '-data', workspace_dir
-  },
-
-  -- 💀
-  -- This is the default if not provided, you can remove it. Or adjust as needed.
-  -- One dedicated LSP server & client will be started per unique root_dir
-  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-
-  -- Here you can configure eclipse.jdt.ls specific settings
-  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-  -- for a list of options
-  settings = {
-    java = {
-    }
-  },
-
-  -- Language server `initializationOptions`
-  -- You need to extend the `bundles` with paths to jar files
-  -- if you want to use additional eclipse.jdt.ls plugins.
-  --
-  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-  --
-  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
-  init_options = {
-    bundles = {}
+    -- Use the workspace_folder defined above to store data for this project
+    '-data', workspace_folder,
   },
 }
--- This starts a new client & server,
--- or attaches to an existing client & server depending on the `root_dir`.
-require('jdtls').start_or_attach(jdtls_config)
+
+-- Finally, start jdtls. This will run the language server using the configuration we specified,
+-- setup the keymappings, and attach the LSP client to the current buffer
+jdtls.start_or_attach(config)
+
